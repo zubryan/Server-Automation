bc9ea471e94c55c17fca7399336539cfb7143cb3
Bryan Zhu <zwgzu82@gmail.com>
2012-07-29 21:24:28 +0800

    modify structure of managed_server table

--- sql/sa.sql
+++ sql/sa.sql
@@ -1,9 +1,11 @@
 CREATE TABLE `managed_server` (
     `id` char(32) NOT NULL,
+    `pid` char(32) NOT NULL DEFAULT '0',
+    `type` char(50) NOT NULL,
     `hostname` char(255) NOT NULL,
     `ip` char(16) NOT NULL,
     `status` tinyint(2) NOT NULL,
-    `update_time` timestamp not null,
+    `update_time` timestamp NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -18,6 +20,6 @@ CREATE TABLE `server_software` (
     `id` char(32) NOT NULL,
     `sw_name` char(255) NOT NULL,
     `status` tinyint(2) NOT NULL,
-    `update_time` timestamp not null,
+    `update_time` timestamp NOT NULL,
     PRIMARY KEY (`id`,`sw_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
